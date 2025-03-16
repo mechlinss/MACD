3ad42f54fb10c58e00856dfd9bf6011b7bdd7e28
mechlinss <filip.mechlinski15@gmail.com>
2025-03-17 00:29:47 +0100

    MACD and Signal plots added

--- macd.py
+++ macd.py
@@ -2,44 +2,79 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def calculate_ema(N, data):
-    alpha = 2/(N + 1)
-    factors = (1 - alpha) ** np.arange(N + 1)
-    factors = factors[::-1]
+def calculate_ema(n, data):
+    alpha = 2 / (n + 1)
     result = np.nan * np.zeros_like(data)
-    for i in range(N, len(data)):
-        result[i] = np.sum(data[i - N:i + 1] * factors) / factors.sum()
+    result[0] = data[0]
+
+    for i in range(1, len(data)):
+        result[i] = (alpha * data[i] + (1 - alpha) * result[i - 1])
+
     return result
 
 extracted_data = pd.read_csv("data/eth_v_d.csv")
 
 extracted_data["Data"] = pd.to_datetime(extracted_data["Data"])
-date = extracted_data[["Data"]]
+date = extracted_data["Data"]
 close = extracted_data[["Zamkniecie"]]
 
-plt.plot(date, close, color='b')
+plt.figure(figsize=(15, 7))
+plt.plot(date, close.values, color='b')
 plt.ylabel("Price [USD]")
 plt.xlabel("Date")
-plt.title("Ethereum price in 3 years period")
+plt.title("Ethereum price over 3 years")
 plt.grid(True)
 plt.show()
 
-macd = calculate_ema(data['Close'].to_numpy(), 12) - calculate_ema(data['Close'].to_numpy(), 26)
-signal = calculate_ema(macd, 9)
+macd = calculate_ema(12, close.to_numpy()) - calculate_ema(26, close.to_numpy())
+signal = calculate_ema(9, macd)
 
-data.insert(0, 'MACD', macd, allow_duplicates=True)
-data.insert(1, 'Signal', signal, allow_duplicates=True)
-
-# Select last 1000 records
-data = data[-1000:]
-
-plt.figure().set_figwidth(15)
-plt.plot(data['Date'], data['Close'], label=STOCK_NAME)
-plt.xlabel('Data')
-plt.ylabel(f'Cena [{STOCK_CURRENCY}]')
-plt.title(f'{PLOT_TITLE} od {data.iloc[0]["Date"].date()} do {data.iloc[-1]["Date"].date()}')
+plt.figure(figsize=(15, 7))
+plt.plot(date, macd, label="MACD", color='b')
+plt.plot(date, signal, label="SIGNAL", color='r')
+plt.axhline(0, color='black', linestyle='--', linewidth=0.5)
+plt.xlabel("Date")
+plt.ylabel("MACD/Signal Value")
+plt.title("MACD and Signal Line for ETH")
 plt.legend()
-plt.savefig(f'{IMAGES_PATH}part_pln.png', bbox_inches='tight')
+plt.grid(True)
+plt.show()
+
+buy_values = []
+buy_dates = []
+sell_values = []
+sell_dates = []
+
+for i in range(0, len(macd) - 1):
+    if signal[i] > macd[i] and signal[i + 1] < macd[i + 1]:
+        buy_values.append(macd[i + 1])
+        buy_dates.append(date[i + 1])
+    elif signal[i] < macd[i] and signal[i + 1] > macd[i + 1]:
+        sell_values.append(macd[i + 1])
+        sell_dates.append(date[i + 1])
+
+
+plt.figure(figsize=(15, 7))
+plt.plot(date, macd, label="MACD", color='b')
+plt.plot(date, signal, label="Signal", color='orange')
+plt.axhline(0, color='black', linestyle='--', linewidth=0.5)
+plt.scatter(buy_dates, buy_values, color='green', marker='^', label='BUY', s=75)
+plt.scatter(sell_dates, sell_values, color='red', marker='v', label='SELL', s=75)
+plt.xlabel("Date")
+plt.ylabel("MACD/Signal Value")
+plt.title("MACD and Signal Line with Buy/Sell Signals")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(15, 7))
+plt.plot(date, close.values, color='b')
+plt.scatter(buy_dates, buy_values, color='green', marker='^', label='BUY', s=75)
+plt.scatter(sell_dates, sell_values, color='red', marker='v', label='SELL', s=75)
+plt.ylabel("Price [USD]")
+plt.xlabel("Date")
+plt.title("Ethereum price over 3 years")
+plt.grid(True)
 plt.show()
 
 print(date.head())
